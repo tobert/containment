759d45e365c525454654a0a4cd84595669fdc9e4
Al Tobey <tobert@gmail.com>
2011-08-21 21:11:57 -0700

    switch call style

--- examples/nsfork_only.rb
+++ examples/nsfork_only.rb
@@ -2,7 +2,7 @@ require 'containment/ns'
 
 puts "Parent pid is #{Process.pid}"
 
-pid = Containment::NS::nsfork(0)
+pid = Containment::NS.nsfork(0)
 if pid == 0 then
     puts "Process.pid thinks the pid is #{Process.pid}, but that's a lie!"
     begin
