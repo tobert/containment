384a344997d13c4a0b346e98f848a1e0ac141296
Al Tobey <tobert@gmail.com>
2011-08-21 17:42:54 -0700

    Fill in some basic parts of cgroup support.

--- lib/containment/cgroup.rb
+++ lib/containment/cgroup.rb
@@ -1,14 +1,47 @@
 module Containment
   class Cgroup
     attr_reader :name
+    attr_reader :path
 
     def initialize(cgroup_name)
       @name = cgroup_name
+      @path = "/cgroup/#{@name}"
+
+      if not Dir.exists?(@path)
+        Dir.mkdir(@path)
+      end
     end
 
+    # Attaches the +pid+ specified to the cgroup.
+    def attach_pid(pid)
+      File.open(File.join(@path, 'tasks'), "w") do |tasks|
+        tasks.puts(pid)
+      end
+    end
+
+    # Returns a list of pids attached to the cgroup.
     def tasks
-      File.read(File.join($CGROUP_ROOT)
-      raise "Stub! But this one is essential!"
+      task_list = []
+      File.open(File.join(@path, 'tasks'), "r") do |tasks|
+        pid = tasks.gets.chomp
+        task_list.push pid
+      end
+      return task_list
+    end
+
+    # Returns a list of child cgroups.
+    def children
+      children = []
+      Dir.open(@path) do |file|
+        next if file.start_with?(".")
+
+        cpath = File.join(@path, file)
+
+        if File.ftype(cpath) == "directory" then
+          cg = Containment::Cgroup.new(cpath)
+          children.push(cg)
+        end
+      end
     end
 
     def notify_on_release
@@ -18,6 +51,19 @@ module Containment
     def release_agent
       raise "Stub!"
     end
+
+    def cgroup_clone_children
+      raise "Stub!"
+    end
+
+    def cgroup_event_control
+      raise "Stub!"
+    end
+
+    def cgroup_procs
+      raise "Stub!"
+    end
+
   end
 end
 
