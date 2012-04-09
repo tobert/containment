31beac0fe571a151db377d7d7b69df74083aff20
Al Tobey <tobert@gmail.com>
2012-04-08 20:29:42 -0700

    Switch cgroups to a method_missing.
    Lets me be lazier at the expense of a fairly nasty bit of trickery. It should also
    generally handle changes to cgroups over time, so that's a bonus.

--- lib/containment/cgroup.rb
+++ lib/containment/cgroup.rb
@@ -1,133 +1,105 @@
-require 'containment/util'
-
 module Containment 
   class Cgroup
-    include Containment::Util
+    attr_reader :name, :path
 
-    attr_reader :name
-    attr_reader :path
-
-    def initialize(cgroup_name)
-      if not File.readable?("/cgroup/tasks")
-        raise "Cannot read /cgroup/tasks. Cgroups is not available."
-      end
-
-      if cgroup_name.start_with?('/cgroup') then
-        parts = cgroup_name.split(/\/+/)
-        parts.delete_at(0)
-        parts.delete_at(0)
-        @name = File.join(parts)
-      else
-        @name = cgroup_name
-      end
-      @path = File.join('/cgroup', @name)
+    def initialize(name, path)
+      @name = name
+      @path = path
 
       if not File.directory?(@path)
         Dir.mkdir(@path)
       end
     end
 
-    # Attaches the +pid+ specified to the cgroup.
-    def attach_pid(pid)
-      File.open(File.join(@path, 'tasks'), "w") do |tasks|
-        tasks.puts(pid)
-      end
-    end
-
-    # Returns a list of pids attached to the cgroup.
-    def tasks
-      task_list = []
-      File.open(File.join(@path, 'tasks'), "r") do |t|
-        while (task = t.gets)
-          task.chomp!
-          task_list.push task
+    # Attaches the +pid+ and its threads specified to the cgroup.
+    def attach(pid)
+      # all of a process's threads are in /proc/<pid>/task/<number>
+      Dir.open("/proc/#{pid}/task").each do |task|
+        File.open(File.join(@path, 'tasks'), "w") do |f|
+          f.puts(pid)
         end
       end
-      return task_list
     end
 
-    # Returns a list of child cgroups.
     def children
-      children = []
-      Dir.foreach(@path) do |file|
-        next if file.start_with?(".")
-
-        cpath = File.join(@path, file)
-
-        if File.ftype(cpath) == "directory" then
-          cg = Containment::Cgroup.new(cpath)
-          children.push(cg)
+      out = []
+      Dir.open(@path).each do |file|
+        next if file == '.' or file == '..'
+        path = File.join(@path, file)
+        if File.readable? File.join(path, 'tasks')
+          out << path
         end
       end
-      return children
+      out
     end
 
-    def blkio
-      obj = Containment::Cgroup::BlkIO.new(self)
-      if block_given?
-        yield obj
-      else
-        return obj
+    #
+    # The majority of the useful bits of cgroups (and proc/sys for that matter) use simple single values
+    # except for where the kernel devs felt like trolling poor tools people like me.
+    # This method_missing tries to cover most of the common stuff and must be overridden for things that
+    # need more parsing.
+    #
+    def method_missing(method, *args)
+      name = method.to_s.sub(/=/, '')
+      path = File.join(@path, name)
+
+      # cgroup filenames with dots in them get translated into snake case, e.g.
+      # blkio.weight -> cg.blkio_weight = 100
+      snakename = name.gsub("_", ".")
+      snake = File.join(@path, snakename)
+      if File.exists?(snake) and not File.exists?(path)
+        path = snake
       end
-    end
 
-    def cpu
-      obj = Containment::Cgroup::CPU.new(self)
-      if block_given?
-        yield obj
-      else
-        return obj
+      if File.directory? path
+        return Cgroup.new name, path
       end
-    end
 
-    def cpuacct
-      obj = Containment::Cgroup::CPUAcct.new(self)
-      if block_given?
-        yield obj
-      else
-        return obj
+      unless File.exists? path
+        raise "No such method: #{method}. Tried to access '#{path}'"
       end
-    end
-
-    def cpuset
-      obj = Containment::Cgroup::CPUSet.new(self)
-      if block_given?
-        yield obj
-      else
-        return obj
+      
+      if method.to_s.end_with?('=')
+        raise "#{path} is not writable" unless File.writable? path
+        File.open(path, "w") do |fd|
+          args.each do |a| fd.puts a end
+        end
       end
-    end
 
-    def devices
-      obj = Containment::Cgroup::Devices.new(self)
-      if block_given?
-        yield obj
-      else
-        return obj
-      end
-    end
+      raise "#{path} is not readable" unless File.readable? path
+      value = File.read(path).chomp
 
-    def memory
-      obj = Containment::Cgroup::Memory.new(self)
-      if block_given?
-        yield obj
-      else
-        return obj
-      end
-    end
+      # single-entry file with a single number
+      if value =~ /\A\d+\Z/
+        value.to_i
+      # multi-line files
+      elsif value =~ /\n/
+        items = value.split(/\n+/)
 
-    def notify_on_release
-      slurp_int(File.join(@path, 'notify_on_release')) do |val|
-        if val.to_i == 0
-          return false
+        # key = value or key[key] = value files
+        # e.g. blkio.time = { '8:11' => 1234 }
+        # e.g. blkio.throttle.io_serviced = { '8:0' => { 'Read' => 1234 } }
+        if items.any? { |i| i =~ /\s+/ }
+          out = {}
+          items.each do |line|
+            parts = line.chomp.split(/\s+/)
+            if parts.count == 2
+              out[parts[0]] = parts[1]
+            elsif parts.count == 3
+              out[parts[0]][parts[1]] = parts[2]
+            else
+              raise "list is too big and unsupported"
+            end
+          end
+          out
+        # it's just a flat list of values, try to convert numbers to fixnum and return
         else
-          return true
+          items.map { |i| i.=~(/\A\d+\Z/) ? i.to_i : i }
         end
+      # return the data unmolested
+      else
+        value
       end
-    end
-
-    def release_agent
-      raise "Stub!"
     end
   end
 end
