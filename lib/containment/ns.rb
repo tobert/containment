83df0a06d7db031610aa4be1ba3b44177514f3ae
Al Tobey <tobert@gmail.com>
2012-04-08 19:39:04 -0700

    Switch exclusively to FFI, clean up

--- lib/containment/ns.rb
+++ lib/containment/ns.rb
@@ -1,60 +1,23 @@
-require 'containment/linux_constants'
-require 'containment/linux_syscall'
-
-# TODO: it might be best to have this fall back to regular fork()
-# on non-linux platforms or linux kernels without NS support to
-# make testing & using dependents on this module on those platforms
-# easier
+require 'containment/constants'
+require 'containment/syscall'
 
 module Containment
-  module NS
+  # namespace enabled fork()-ish function
+  # pid = nsfork()
+  #
+  # to create a network namespace, add the CLONE_NEWNET flag
+  # you will have to set up veth or whatever manually
+  # pid = nsfork(Containment::Linux::Sched::CLONE_NEWNET)
+  # 
+  def self.nsfork(extra_flags=0)
+    clone_flags = extra_flags \
+                | Containment::Linux::Sched::CLONE_NEWNS \
+                | Containment::Linux::Sched::CLONE_NEWIPC \
+                | Containment::Linux::Sched::CLONE_NEWPID \
+                | Containment::Linux::Sched::CLONE_NEWUTS \
+                | Signal.list["CHLD"]
 
-    # the syscall number for SYS_clone is platform-dependent and even
-    # runtime-dependent (e.g. 32-bit ruby on 64-bit Linux needs to call
-    # the 32-bit syscall, not 64bit) This cheeziness should be good
-    # enough for now, but is nowhere near ideal.
-    
-    if RUBY_PLATFORM == "x86_64-linux"
-      @sys_clone_nr  = Containment::Linux::ASM_X86_64::Unistd::NR_clone
-      @sys_getpid_nr = Containment::Linux::ASM_X86_64::Unistd::NR_getpid
-    # TODO: I don't have a 32bit box handy, make sure this is right ...
-    else
-      @sys_clone_nr  = Containment::Linux::ASM_X86_32::Unistd::NR_clone
-      @sys_getpid_nr = Containment::Linux::ASM_X86_32::Unistd::NR_getpid
-    end
-
-    # Process.pid lies. Go to the source for truth.
-    module_function
-    def getpid
-      Containment::Linux::syscall(@sys_getpid_nr, 0, 0)
-    end
-
-    # namespace enabled fork()-ish function
-    # pid = nsfork()
-    #
-    # to create a network namespace, add the CLONE_NEWNET flag
-    # you will have to set up veth or whatever manually
-    # pid = nsfork(Containment::Linux::Sched::CLONE_NEWNET)
-    # 
-    module_function
-    def nsfork(extra_flags=0)
-      clone_flags = extra_flags \
-                  | Containment::Linux::Sched::CLONE_NEWNS \
-                  | Containment::Linux::Sched::CLONE_NEWIPC \
-                  | Containment::Linux::Sched::CLONE_NEWPID \
-                  | Containment::Linux::Sched::CLONE_NEWUTS \
-                  | Containment::Linux::ASM_Generic::Signal::SIGCHLD
-      sys_clone(clone_flags)
-    end
-
-    # you probably don't want to call this
-    # see nsfork() above - it could be handy if you want to dink with
-    # totally custom sets of flags
-    def sys_clone(flags)
-      # see clone(2) for a description of SYS_clone
-      Containment::Linux::syscall(@sys_clone_nr, flags, 0)
-    end
-
+    Containment::Syscall.sys_clone(clone_flags)
   end
 end
 
--- lib/containment/syscall.rb
+++ lib/containment/syscall.rb
@@ -1,102 +1,28 @@
-require 'containment/linux_constants'
-
-# This is gross and disgusting. libffi support is stubbed out and not
-# implemented.
-# The syscall() that's exposed only supports 3-arg sys_clone() since
-# that's all I need and doing full vararg & copy_(to|from)_user would
-# be pretty duanting.
-#
-# It happens to work for getpid too, which is quite handy since Ruby
-# caches Process.pid which is totally wrong for clone.
+require 'containment/constants'
+require 'ffi'
 
 module Containment
-  module Linux
-    @syscall_func = lambda { |a,b,c| raise "could not syscall - platform probably isn't supported" }
+  module Syscall
+    extend FFI::Library
 
-    #  @libc_so is used to for DL.dlopen
-    #  @syscall_getpid is used to test syscall()
-    #  @syscall_func is the backend function
+    ffi_lib [FFI::CURRENT_PROCESS, 'c']
 
-    # cribbed from ruby-1.9.2-p290/test/fiddle/helper.rb
-    case [0].pack('L!').size
+    attach_variable :errno, :int
 
-      # 32-bit untested AFAIK
-      when 4
-        if File.exists?("/lib32/libc.so.6")
-          @libc_so = "/lib32/libc.so.6"
-        end
-
-        # used for testing at end of module init
-        @syscall_getpid = Containment::Linux::ASM_X86_32::Unistd::NR_getpid
-
-      # 64-bit - all of my machines & vm's are currently x86_64
-      when 8
-        if File.exists?("/lib64/libc.so.6")
-          @libc_so = "/lib64/libc.so.6"
-        end
-
-        # used for testing at end of module init
-        @syscall_getpid = Containment::Linux::ASM_X86_64::Unistd::NR_getpid
-
-      # fall back the usual dir/symlink, probably works most of the time
-      else
-        @libc_so = "/lib/libc.so.6"
-    end
-
-    # detect a method for making syscalls
-    # first try Kernel.syscall (ruby <= 1.8)
-    begin
-      Kernel.syscall(@syscall_getpid)
-      @syscall_func = lambda {|syscall, arg0, arg1|
-        Kernel.syscall(syscall, arg0, arg1)
-      }
-    rescue NotImplementedError => wtfbbq
-      # now try fiddle (ruby >= 1.9.2??)
-      begin
-        require 'dl'
-        require 'fiddle'
-
-        # from linux/arch/x86/include/asm/syscalls.h:
-        # long sys_clone(unsigned long, unsigned long, void __user *,
-        #            void __user *, struct pt_regs *);
-        # the last two fields aren't used - the man page says it's ok
-        # to set the first void* to 0 to not set a stack
-        libc = DL.dlopen(@libc_so)
-        @syscall_func = Fiddle::Function.new(
-          libc['syscall'],
-          [
-            Fiddle::TYPE_LONG,
-            Fiddle::TYPE_LONG,
-            Fiddle::TYPE_VOIDP
-          ],
-          Fiddle::TYPE_LONG
-        )
-      rescue
-        # and finally, libffi - should work everywhere, even jruby??
-        begin
-          require 'libffi'
-          raise "BUG: Fell back to libffi succesfully, but support is not implemented yet!"
-        rescue
-          raise "Could not load an interface for making syscalls. Tried Kernel.syscall, fiddle, and libffi."
-        end
+    # only set up a 2-argument version, works fine for both of my use cases
+    attach_function :sys_syscall, :syscall, [:long, :ulong, :ulong], :long
+    
+    def self.sys_clone(flags)
+      rc = sys_syscall(Containment::ASM::Unistd::NR_clone, flags, 0)
+      if rc < 0
+        raise "sys_clone() failed."
       end
+      rc
     end
 
-
-    # this is a limited interface to syscall() that doesn't have
-    # to mess around with varargs
-    module_function
-    def syscall(nr, arg0, arg1)
-      @syscall_func.call(nr, arg0, arg1)
+    def self.getpid
+      rc = sys_syscall(Containment::ASM::Unistd::NR_getpid, 0, 0)
     end
-
-    begin
-      pid = Containment::Linux::syscall(@syscall_getpid, 0, 0)
-      #puts pid # uncomment for a quick test
-    rescue
-      raise "Containment::Linux::syscall() failed at bootstrap testing. Is your platform supported?"
-    end
-
   end
 end
 
